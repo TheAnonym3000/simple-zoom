14922ad931d6c001f0a3c7646dd3c4a1642009b1
TheAnonym3000 <melchior.voigts@ewe.net>
2024-12-26 18:05:46 +0100

    Adding keybind and looking for zoom

--- src/client/java/xyz/anonym/simple_zoom/client/Simple_zoomClient.java
+++ src/client/java/xyz/anonym/simple_zoom/client/Simple_zoomClient.java
@@ -1,20 +1,34 @@
 package xyz.anonym.simple_zoom.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricAdvancementProvider;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
-import net.minecraft.text.Text;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Simple_zoomClient implements ClientModInitializer {
     public static final String MOD_ID = "simple-zoom";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public  static KeyBinding keyBinding;
 
     @Override
     public void onInitializeClient() {
         LOGGER.info("Let it be zoomy! (and more readable (the signs))");
-
+        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.simple-zoom.zoom",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_Z,
+                "category.simple-zoom"
+        ));
+        ClientTickEvents.END_CLIENT_TICK.register( client -> {
+            while (keyBinding.wasPressed()) {
+                //TODO
+                LOGGER.info("pressed zoom key");
+            }
+        });
     }
 
 }
---
+++ src/client/java/xyz/anonym/simple_zoom/client/mixins/GameRendererMixin.java
@@ -0,0 +1,9 @@
+package xyz.anonym.simple_zoom.client.mixins;
+
+import net.minecraft.client.render.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+    //S
+}
